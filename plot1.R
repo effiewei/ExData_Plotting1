5719d257b754f2b75f9f454fb5078781ee6fb082
effiewei <EffieWei@giichis-MacBook-Air.local>
2017-01-15 12:28:19 -0500

    plot1

---
+++ plot1.R
@@ -0,0 +1,12 @@
+setwd("/Volumes/Transcend/Statistics Language/R/Coursera/Exploratory Data Analysis with R/Week1")
+file <- read.csv2("household_power_consumption.txt")
+file$Date <- as.Date(file$Date, format="%d/%m/%Y")
+data <- file[file$Date == "2007-02-01" | file$Date == "2007-02-02", ]
+data2 <- data.frame(data$Global_active_power)
+f <- data$Global_active_power
+newf <- as.numeric(levels(f))[f]
+png('plot1.png')
+hist(newf, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
+
+
